e0374a488d198e24b9467ea328582305bbef20d1
pashakxpy1 <pasha.kxpay@gmail.com>
2025-01-26 18:27:07 +0000

    кінець урока 1

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-##
+print("Hello Replit")
+print("Well we")
+print("just use more lines")
+print("of code")
+print("""Anything that starts
+with three quotes, and ends
+in three quotes can span
+many lines and even contain " symbols
+within it without freaking anything out!""")
+print("""=======================================================================""")
+print("= MUSIC+ =")
+print("> Songs")
+print("> Albums")
+print("> Artists")
+
+
+print("Marky")
+print("September 20, 2029")
+
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!""")
+
+print("I am feeling 🥳")
+
+print("You can follow my progress at replit.com/IamMarkProbably")
---
+++ readme.md
@@ -0,0 +1,13 @@
+#  урок 1
+
+В цьому уроці цікаве тільки print multyline.
+Те що нижче
+
+```py
+
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!""")
+
+
+```
